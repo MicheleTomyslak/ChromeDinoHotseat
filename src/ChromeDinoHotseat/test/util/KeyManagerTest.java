476d4d23035c180d949f9333a061d79e799739fa
Michele Tomyslak <37552579+MicheleTomyslak@users.noreply.github.com>
2025-06-10 11:42:01 +0200

    Clean up unit test boilerplate

--- src/ChromeDinoHotseat/test/util/KeyManagerTest.java
+++ src/ChromeDinoHotseat/test/util/KeyManagerTest.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package util;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.awt.event.KeyEvent;
@@ -18,32 +9,12 @@ import java.awt.event.KeyEvent;
  * @author michele.tomyslak
  */
 public class KeyManagerTest {
-    
-    public KeyManagerTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of getDuckKey method, of class KeyManager.
      */
     @Test
     public void testGetDuckKey() {
-        System.out.println("getDuckKey");
         KeyManager instance = new KeyManager();
         int result = instance.getDuckKey();
         assertEquals(KeyEvent.VK_DOWN, result);
@@ -54,7 +25,6 @@ public class KeyManagerTest {
      */
     @Test
     public void testGetJumpKey() {
-        System.out.println("getJumpKey");
         KeyManager instance = new KeyManager();
         int result = instance.getJumpKey();
         assertEquals(KeyEvent.VK_SPACE, result);
--- src/ChromeDinoHotseat/test/util/UtilSuite.java
+++ src/ChromeDinoHotseat/test/util/UtilSuite.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package util;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- *
- * @author michele.tomyslak
- */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     util.KeyManagerTest.class,
@@ -26,21 +13,5 @@ import org.junit.runners.Suite;
     util.ConfigTest.class,
     util.ScoreTest.class})
 public class UtilSuite {
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
     
 }
